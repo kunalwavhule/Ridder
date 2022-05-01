363f31dfe1930a00b521304e73a075dffff8c64b
anmolmadaan368 <[anmolmad503@gmail.com]>
2022-05-01 12:41:11 +0530

    basic functionality

--- app/src/main/java/com/example/rider/SignUpActivity.java
+++ app/src/main/java/com/example/rider/SignUpActivity.java
@@ -2,13 +2,28 @@ package com.example.rider;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        btnContinue = findViewById(R.id.btnContinue);
+
+        btnContinue.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(getApplicationContext(), MapActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/rider/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.example.rider;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
+    }
+}
